87dc42bae1a9412a04171c5d950c82d445502335
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-11-26 13:30:07 +0100

    Refactor guess method and add constants

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -2,6 +2,10 @@ class Codebreaker
   class Game
     attr_reader :output
 
+    LENGTH_OF_SECRET_NUMBER = 4
+    INDEX_START = 0
+    INDEX_END = 3
+
     def initialize(output)
       @output = output
     end
@@ -15,26 +19,40 @@ class Codebreaker
 
     def guess(input)
       hint = ''
+      @matched_numbers = []
 
-      if input.length != 4
+      if input.length != LENGTH_OF_SECRET_NUMBER
         output.puts "Try guessing a number with four digits"
         return
       end
 
-      matched_numbers = []
-
-      for number in 0..3
+      for number in INDEX_START..INDEX_END
         current_input = input[number]
         current_secret = @secret_number[number]
-        if current_input == current_secret
+        if exact_match(current_input, current_secret)
           hint += '+'
-        elsif (@secret_number.include? current_input) && (!matched_numbers.include? current_input)
+        elsif number_match(current_input, current_secret)
           hint += '-'
         end
-        matched_numbers << current_input
+        @matched_numbers << current_input
       end
 
+      print(hint)
+    end
+
+    private
+
+    def exact_match(current_input, current_secret)
+      current_input == current_secret
+    end
+
+    def number_match(current_input, current_secret)
+      (@secret_number.include? current_input) && (!@matched_numbers.include? current_input)
+    end
+
+    def print(hint)
       output.puts hint.split('').sort.join
     end
+
   end
 end
