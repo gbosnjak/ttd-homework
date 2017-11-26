61a8ade21d7234bf5cb77cc620f4676b28a40d32
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-11-26 13:47:45 +0100

    More refactoring

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -18,7 +18,7 @@ class Codebreaker
     end
 
     def guess(input)
-      hint = ''
+      @hint = ''
       @matched_numbers = []
 
       if input.length != LENGTH_OF_SECRET_NUMBER
@@ -26,33 +26,45 @@ class Codebreaker
         return
       end
 
-      for number in INDEX_START..INDEX_END
-        current_input = input[number]
-        current_secret = @secret_number[number]
-        if exact_match(current_input, current_secret)
-          hint += '+'
-        elsif number_match(current_input, current_secret)
-          hint += '-'
-        end
-        @matched_numbers << current_input
-      end
-
-      print(hint)
+      check_exact_matches(input)
+      check_number_matches(input)
+      print_hint
     end
 
     private
 
+    def check_exact_matches(input)
+      for number in INDEX_START..INDEX_END
+        current_input = input[number]
+        current_secret = @secret_number[number]
+        if exact_match(current_input, current_secret)
+          @hint += '+'
+          @matched_numbers << current_input
+        end
+      end
+    end
+
     def exact_match(current_input, current_secret)
       current_input == current_secret
+    end
+
+    def check_number_matches(input)
+      for number in INDEX_START..INDEX_END
+        current_input = input[number]
+        current_secret = @secret_number[number]
+        if number_match(current_input, current_secret)
+          @hint += '-'
+        end
+        @matched_numbers << current_input
+      end
     end
 
     def number_match(current_input, current_secret)
       (@secret_number.include? current_input) && (!@matched_numbers.include? current_input)
     end
 
-    def print(hint)
-      output.puts hint.split('').sort.join
+    def print_hint
+      output.puts @hint.split('').sort.join
     end
-
   end
 end
